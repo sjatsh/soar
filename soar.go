41b8ec4733027b565e120e710efea09cb066e106
june <i@sjis.me>
2019-11-14 17:52:04 +0800

    1

---
+++ soar.go
@@ -0,0 +1,10 @@
+// Author: SunJun <i@sjis.me>
+package soar
+
+import (
+	_ "github.com/sjatsh/soar/advisor"
+	_ "github.com/sjatsh/soar/ast"
+	_ "github.com/sjatsh/soar/common"
+	_ "github.com/sjatsh/soar/database"
+	_ "github.com/sjatsh/soar/env"
+)
